1b9f3dcb5f71a04bde372ab9cd16fa2fed0406b5
taro@diasporajp <taro@diasporajp>
2014-06-25 12:24:00 +0900

    set asset compile true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,8 @@ Diaspora::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+  #config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
